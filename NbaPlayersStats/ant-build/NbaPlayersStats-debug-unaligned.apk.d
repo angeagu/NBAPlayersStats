7352ac4b67ceb1e14ce6e6da6826fa77767d35a7
aaguado <angel.aguado@kisters.es>
2016-03-14 09:54:54 +0100

    First Version with Dojox Mobile

--- NbaPlayersStats/ant-build/NbaPlayersStats-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Documents and Settings\nombre\Escritorio\Pruebas\pruebaAndroid_ws\nba\nbaplayersstats\platforms\android\ant-build\NbaPlayersStats-debug-unaligned.apk : \
-C:\Documents and Settings\nombre\Escritorio\Pruebas\pruebaAndroid_ws\nba\nbaplayersstats\platforms\android\ant-build\NbaPlayersStats.ap_ \
-C:\Documents and Settings\nombre\Escritorio\Pruebas\pruebaAndroid_ws\nba\nbaplayersstats\platforms\android\ant-build\classes.dex \
-C:\Documents and Settings\nombre\Escritorio\Pruebas\pruebaAndroid_ws\nba\nbaplayersstats\platforms\android\CordovaLib\ant-build\classes.jar \
