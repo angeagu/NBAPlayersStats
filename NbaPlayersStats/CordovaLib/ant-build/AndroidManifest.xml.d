72b6aea07cc57b53faa6db709011ba63164bb7f4
aaguado <angel.aguado@kisters.es>
2016-03-14 09:57:21 +0100

    First Version with Dojox Mobile

--- NbaPlayersStats/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Documents and Settings\nombre\Escritorio\Pruebas\pruebaAndroid_ws\nba\nbaplayersstats\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Documents and Settings\nombre\Escritorio\Pruebas\pruebaAndroid_ws\nba\nbaplayersstats\platforms\android\CordovaLib\AndroidManifest.xml \
